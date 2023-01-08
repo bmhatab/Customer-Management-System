b3b664bc89ced1df3a11959093fb13092aa6a8a5
serendi <bmhatab@gmail.com>
2023-01-08 23:18:51 +0800

    Added Customers features and Add Interaction

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app import db,login_manager
 from . import main
 from .forms import UserForm,LoginForm
-from ..forms import CustomerForm
+from ..forms import CustomerForm,InteractionForm
 from ..models import Users,Customer,Interaction
 from flask import Flask, render_template,flash,request,redirect,url_for,session
 from werkzeug.security import generate_password_hash,check_password_hash
@@ -92,3 +92,40 @@ def view_customer(customer_id):
     interactions = Interaction.query.filter_by(customer_id=customer_id).all()
     return render_template('customers/view.html', customer=customer, interactions=interactions)
 
+
+@main.route('/customers/<int:customer_id>/edit/', methods=['GET', 'POST'])
+def edit_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    form = CustomerForm(obj=customer)
+    if form.validate_on_submit():
+        customer.name = form.name.data
+        customer.email = form.email.data
+        customer.phone = form.phone.data
+        customer.address = form.address.data
+        customer.notes = form.notes.data
+        db.session.commit()
+        flash('Customer edited successfully!')
+        return redirect(url_for('main.view_customer', customer_id=customer_id))
+    return render_template('customers/edit.html', form=form, customer=customer)
+
+
+@main.route('/customers/<int:customer_id>/delete/', methods=['GET','POST'])
+def delete_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    db.session.delete(customer)
+    db.session.commit()
+    flash('Customer deleted successfully!')
+    return redirect(url_for('main.list_customers'))
+
+#####################################################################################
+
+@main.route('/customers/<int:customer_id>/interactions/new/', methods=['GET', 'POST'])
+def create_interaction(customer_id):
+    form = InteractionForm()
+    if form.validate_on_submit():
+        interaction = Interaction(customer_id=customer_id, interaction_type=form.interaction_type.data, interaction_date=form.interaction_date.data, notes=form.notes.data)
+        db.session.add(interaction)
+        db.session.commit()
+        flash('Interaction created successfully!')
+        return redirect(url_for('main.view_customer', customer_id=customer_id))
+    return render_template('customers/new_interaction.html',form=form,customer_id=customer_id)
